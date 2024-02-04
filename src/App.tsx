9a7c472bbe1aeb426476c353a42b9feca8ca5fb5
Beksultan Chomoev <john.doe@test.com>
2024-02-04 13:26:55 -0600

    Был создан NavigationBar и добавлен в App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import NavigationBar from './container/NavigationBar.tsx';
+import PersonalInfo from './container/AboutMe.tsx';
+import {Route, Routes} from 'react-router-dom';
+
 function App() {
 
   return (
     <>
+      <header>
+        <NavigationBar />
+      </header>
+      <main className='container-fluid'>
+        <Routes>
+          <Route/>
+          <Route/>
+          <Route/>
+          <Route/>
+        </Routes>
+      </main>
     </>
   );
 }
---
+++ src/container/NavigationBar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const NavigationBar: React.FC = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <span className="navbar-brand "> <h1 className='display-3'>Beksultan Chomoev</h1></span>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-link">
+            <NavLink to="/" className="nav-link">About me</NavLink>
+          </li>
+          <li className="nav-link">
+            <NavLink to="contacts" className="nav-link">Contacts</NavLink>
+          </li>
+          <li className="nav-link">
+            <NavLink to="resume" className="nav-link">Resume</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
